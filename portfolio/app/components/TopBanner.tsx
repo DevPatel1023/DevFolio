5a6cf4efd0a5f1ffa863add9cc19b6ac65f7223d
DevPatel1023 <devp4143@gmail.com>
2025-08-26 11:31:51 +0530

    working on topbanner for image caarousal but img isnt visible

--- portfolio/app/components/TopBanner.tsx
+++ portfolio/app/components/TopBanner.tsx
@@ -1,8 +1,48 @@
+"use client"
+
+import Image from "next/image";
+import { useEffect, useState } from "react"
+
+const images : string[] = [
+  // './img1.jpg',
+  '/img2.jpg',
+  '/img3.jpg',
+  '/img4.jpg',
+  '/img5.jpg'
+]
+
 const TopBanner = ({}) => {
+  const [imgIndex,setImgIndex] = useState<number>(0);
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setImgIndex((prev) => (prev+1) % images.length)
+    },4000);
+
+    return ()=> clearInterval(interval);
+  },[]);
+
   return (
-    <div>
+    <div className="relative w-full h-[400px] overflow-hidden">
         {/* banner img */}
-        <img src="" alt="banner-img" />
+        {images.map((src, i) => (
+        <div
+          key={i}
+          className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 ${
+            i === imgIndex ? 'opacity-100 z-10' : 'opacity-0 z-0'
+          }`}
+        >
+          {/* Log to verify path */}
+          {console.log('Rendering image:', src)}
+          <Image
+            src={src}
+            alt={`Slide ${i + 1}`}
+            fill
+            className="object-cover"
+            priority={i === 0}
+          />
+        </div>
+      ))}
     </div>
   )
 }
