38572c5f35c7297f199ed708ea85837625ecc34f
DevPatel1023 <devp4143@gmail.com>
2025-08-27 22:08:59 +0530

    banner styling

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,7 +2,7 @@ import MainSection from "./pages/MainSection";
 
 export default function Home() {
   return (
-    <div className="font-sans flex items-center justify-center min-h-screen ">
+    <div className="font-sans flex  justify-center bg-background min-h-screen">
       <MainSection />
     </div>
   );
