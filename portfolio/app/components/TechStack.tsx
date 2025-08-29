3dcc35df4220938cb9f92c99371137df2befeaa7
DevPatel1023 <devp4143@gmail.com>
2025-08-29 20:41:07 +0530

    added tech-icons svgs

---
+++ portfolio/app/components/TechStack.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+const techStack = [
+    // launguages
+    {
+        name : "Javascript" ,
+        category : "launguage" ,
+        icon : "/tech-icons/javascript.svg"
+    }
+]
+
+const TechStack = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TechStack
