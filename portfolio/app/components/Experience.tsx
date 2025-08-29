020979aa3249e8fb6f212c5f626d779e27698ac8
DevPatel1023 <devp4143@gmail.com>
2025-08-29 16:23:05 +0530

    adding contributions part in experience section

--- portfolio/app/components/Experience.tsx
+++ portfolio/app/components/Experience.tsx
@@ -3,46 +3,67 @@ import React from "react";
 
 const Experience = () => {
   return (
-    <div className="w-full px-3 bg-background border border-zinc-900">
-      <h1 className="shimmer mt-3 text-zinc-500 text-xl font-serif font-medium tracking-tight overflow-hidden shine">
+    <div className="w-full bg-background border border-zinc-900">
+      <h1 className="px-3 shimmer mt-3 text-zinc-500 text-xl font-serif font-medium tracking-tight overflow-hidden shine">
         Professional Experience
       </h1>
 
       {/* adding companies details */}
-      <div className="flex flex-col mt-3">
+      <div className="flex flex-col mt-3 px-3">
         {/* company-1 */}
         <div className="flex justify-between py-2 items-center">
           <div className="flex flex-row gap-3">
-          <Avatar className="w-[35px] h-[35px]">
-            <AvatarImage src="/elecon.png" />
-            <AvatarFallback className="text-2xl">Tech.Elecon</AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col">
-            <h1>Tech. Elecon Pvt.Ltd</h1>
-            <p className="text-sm text-zinc-500">Full stack developer Intern</p>
-          </div>
+            <Avatar className="w-[35px] h-[35px]">
+              <AvatarImage src="/elecon.png" />
+              <AvatarFallback className="text-2xl">Tech.Elecon</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <h1>Tech. Elecon Pvt.Ltd</h1>
+              <p className="text-sm text-zinc-500">
+                Full stack developer Intern
+              </p>
+            </div>
           </div>
           <p className="text-sm text-zinc-500">Jan 2025 - April 2025</p>
         </div>
         {/* company-2 */}
         <div className="flex justify-between py-2 items-center">
           <div className="flex flex-row gap-3">
-          <Avatar className="w-[35px] h-[35px]">
-            <AvatarImage src="/elecon.png" />
-            <AvatarFallback className="text-2xl">Tech.Elecon</AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col">
-            <h1>Tech. Elecon Pvt.Ltd</h1>
-            <p className="text-sm text-zinc-500">Frontend developer Intern</p>
-          </div>
+            <Avatar className="w-[35px] h-[35px]">
+              <AvatarImage src="/elecon.png" />
+              <AvatarFallback className="text-2xl">Tech.Elecon</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <h1>Tech. Elecon Pvt.Ltd</h1>
+              <p className="text-sm text-zinc-500">Frontend developer Intern</p>
+            </div>
           </div>
           <p className="text-sm text-zinc-500">May 2024 - June 2024</p>
         </div>
       </div>
 
       {/* details about contributions */}
-      <div>
-        
+      <div className="bg-zinc-900 px-3 text-zinc-500">
+        <h1 className="shimmer mt-3 text-zinc-500 text-xl font-serif font-medium tracking-tight overflow-hidden shine py-2">
+          Technical Contributions
+        </h1>
+        <p className="text-sm py-2">
+          <span className="text-white">At Elecon</span>, I contributed to the
+          Quoteflow project, <span className="text-white">working across the frontend, backend, and database
+          layers</span>. On the frontend, I implemented responsive UI components and
+          improved user experience using modern frameworks. On the backend, I
+          helped develop scalable APIs and integrated business logic with
+          database operations. I also optimized NO-SQL queries and ensured data
+          integrity across workflows.
+        </p>
+        <p className="text-sm py-2">
+          <span className="text-white">At Elecon</span>, I worked as a Summer Intern, leading a small team while
+          also contributing as a <span className="text-white">frontend developer</span>. I was responsible for
+          building responsive components and pages for a social media-style UI
+          platform. My work focused on clean UI/UX, mobile responsiveness, and
+          component reusability. I also coordinated team efforts and ensured
+          smooth collaboration throughout the internship.
+        </p>
       </div>
     </div>
   );
