426f3a14639aaad7b9eba7b8c282e571e3882d54
DevPatel1023 <devp4143@gmail.com>
2025-08-29 16:21:28 +0530

    adding experience section contributions

--- portfolio/app/components/Experience.tsx
+++ portfolio/app/components/Experience.tsx
@@ -8,13 +8,41 @@ const Experience = () => {
         Professional Experience
       </h1>
 
-      {/* adding companies */}
+      {/* adding companies details */}
+      <div className="flex flex-col mt-3">
+        {/* company-1 */}
+        <div className="flex justify-between py-2 items-center">
+          <div className="flex flex-row gap-3">
+          <Avatar className="w-[35px] h-[35px]">
+            <AvatarImage src="/elecon.png" />
+            <AvatarFallback className="text-2xl">Tech.Elecon</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col">
+            <h1>Tech. Elecon Pvt.Ltd</h1>
+            <p className="text-sm text-zinc-500">Full stack developer Intern</p>
+          </div>
+          </div>
+          <p className="text-sm text-zinc-500">Jan 2025 - April 2025</p>
+        </div>
+        {/* company-2 */}
+        <div className="flex justify-between py-2 items-center">
+          <div className="flex flex-row gap-3">
+          <Avatar className="w-[35px] h-[35px]">
+            <AvatarImage src="/elecon.png" />
+            <AvatarFallback className="text-2xl">Tech.Elecon</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col">
+            <h1>Tech. Elecon Pvt.Ltd</h1>
+            <p className="text-sm text-zinc-500">Frontend developer Intern</p>
+          </div>
+          </div>
+          <p className="text-sm text-zinc-500">May 2024 - June 2024</p>
+        </div>
+      </div>
+
+      {/* details about contributions */}
       <div>
-        <Avatar className="w-[35px] h-[35px]">
-          <AvatarImage src="/elecon.png" />
-          <AvatarFallback className="text-2xl">DP</AvatarFallback>
-          
-        </Avatar>
+        
       </div>
     </div>
   );
